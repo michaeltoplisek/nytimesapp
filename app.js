08a0e5c4a9320a563b5574e138f138599800babe
Tops <mtoplisek95@gmail.com>
2018-10-05 12:29:53 -0400

    copied Alex and marks code to the app file which starts the logic for the article search

---
+++ app.js
@@ -0,0 +1,25 @@
+$('#search').on('click', function () {
+    const searchTerm = $('#queryInput').val();
+    const beginTerm = $('#beginInput').val();
+    const endTerm = $('#endInput').val();
+   
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+      'api-key': "e41f32bad21a41b2a8f7ef9830620a24"
+      'q : ${queryInput}'
+      'begin_date : ${beginInput}'
+      'end_date : ${endInput}'
+   
+    });
+    $.ajax({
+      url: url,
+      method: 'GET',
+    }).done(function(result) {
+      console.log(result);
+    }).fail(function(err) {
+      throw err;
+    });
+   
+   
+   
+   });
